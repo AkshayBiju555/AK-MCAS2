303d490cb5d75d0353726b12e1bac81f42c00d04
akshay <akshay>
2026-02-18 14:07:50 +0530

    18-02-2026

---
+++ Java programming/methodoverloading.java
@@ -0,0 +1,49 @@
+/*
+18-02-26
+Method Overriding  
+*/
+
+class methodoverloading {
+
+    int add(int a) {
+        return a + 10;
+    }
+
+    int add(int a, int b) {
+        return a + b;
+    }
+
+    int add(int a, int b, int c) {
+        return a + b + c;
+    }
+
+    double add(double a, double b) {
+        return a + b;
+    }
+
+    double area(double side) {
+        return side * side;
+    }
+
+    double area(double length, double width) {
+        return length * width;
+    }
+
+    double area(double base, double height, double third) {
+        return 0.5 * base * height;
+    }
+
+    public static void main(String[] args) {
+
+        methodoverloading obj = new methodoverloading();
+
+        System.out.println(obj.add(5));
+        System.out.println(obj.add(10, 20));
+        System.out.println(obj.add(1, 2, 3));
+        System.out.println(obj.add(5.5, 4.5));
+
+        System.out.println(obj.area(4));
+        System.out.println(obj.area(5, 4));
+        System.out.println(obj.area(6, 3, 1));
+    }
+}
