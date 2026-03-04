d40d19fa5205b2938069ec94ab0842b401fba9e8
akshay <akshay>
2026-03-04 14:19:19 +0530

    04-03-2026

---
+++ Java programming/sortstring.java
@@ -0,0 +1,52 @@
+/*
+04-03-2026
+
+Q9.Sort Strings */
+
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class sortstring { 
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("How many strings? ");
+        int n = sc.nextInt();
+        sc.nextLine();  // clear buffer
+
+        String[] arr = new String[n];
+
+        for (int i = 0; i < n; i++) {
+            System.out.print("Enter string " + (i + 1) + ": ");
+            arr[i] = sc.nextLine();
+        }
+
+        // Sort in ascending order
+        Arrays.sort(arr);
+
+        System.out.println("Sorted strings:");
+        for (String s : arr) {
+            System.out.println(s);
+        }
+
+        
+    }
+  
+}
+
+
+/*
+
+PS C:\AK\Java programming> javac sortstring.java
+PS C:\AK\Java programming> java sortstring      
+How many strings? 3
+Enter string 1: zebra
+Enter string 2: lion
+Enter string 3: deer
+Sorted strings:
+deer
+lion
+zebra
+*/
