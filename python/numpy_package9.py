edbe5b96231a5b01e8cef97272ba7e5982237b11
akshay <akshay>
2026-08-07 12:08:20 +0530

    09-08-2026\

---
+++ python/numpy_package9.py
@@ -0,0 +1,8 @@
+#write a numpy program to create 4*4 matrix with random values.create a new array from the said array by swapping first and last rows
+import numpy as np
+arr=np.random.random_integers(1,200,16).reshape(4,4)
+print(arr)
+
+
+
+#write a numpy program to multiplyn two given arrays of same size element by element
